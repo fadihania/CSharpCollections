777a289e3096d3d289adf2d604d57f349cc2fb53
Fadi Hania <fhania@gmail.com>
2023-03-08 09:08:39 +0200

    Using arrays and foreach

--- CSharpCollections/Program.cs
+++ CSharpCollections/Program.cs
@@ -1,2 +1,13 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿var colors = new string[] { "RED", "GREEN", "BLUE", "YELLOW", "WHITE", "BLACK" };
+
+//for (var i = 0; i < colorsArr.Length; i++)
+//{
+//    Console.WriteLine(colorsArr[i]);
+//}
+
+foreach (var color in colors)
+{
+    Console.WriteLine(color);
+}
+Console.WriteLine("---------------------------------------------");
+
